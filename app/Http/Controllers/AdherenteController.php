058da493b24514fa8048592af484a8d2155f5bb6
Juan Felipe Arrieta Ortiz <ticsoft.desarrollador@gmail.com>
2024-07-03 19:02:57 -0500

    envio de correos al retirar adherente

--- app/Http/Controllers/AdherenteController.php
+++ app/Http/Controllers/AdherenteController.php
@@ -331,21 +331,21 @@ class AdherenteController extends Controller
                 'Motivo' => $request->Motivo
             ]);
 
-            // $fecha = now()->format('d/m/Y');
-            // $content = <<<HTML
-            //             <h1>Club Sincelejo</h1>
-            //             <p><strong>Fecha:</strong> {$fecha}</p>
-            //             <h3>Cordial saludo,</h3>
-            //             <p>Estimado(a) socio(a),</p>
-            //             <p>Queremos informarle que su estado en el Club Sincelejo ha sido cambiado a <strong>{$estadoString}</strong>.</p>
-            //             <p><strong>Motivo:</strong> {$request->Motivo}</p>
-            //             <p>En caso de inquietudes, no dude en contactar a la gerencia del club.</p>
-            //             <p>Atentamente,<br>
-            //             Gerencia<br>
-            //             Club Sincelejo</p>
-            //             HTML;
+            $fecha = now()->format('d/m/Y');
+            $content = <<<HTML
+                        <h1>Club Sincelejo</h1>
+                        <p><strong>Fecha:</strong> {$fecha}</p>
+                        <h3>Cordial saludo,</h3>
+                        <p>Estimado(a) socio(a),</p>
+                        <p>Queremos informarle que su estado en el Club Sincelejo ha sido cambiado a <strong>{$estadoString}</strong>.</p>
+                        <p><strong>Motivo:</strong> {$request->Motivo}</p>
+                        <p>En caso de inquietudes, no dude en contactar a la gerencia del club.</p>
+                        <p>Atentamente,<br>
+                        Gerencia<br>
+                        Club Sincelejo</p>
+                        HTML;
 
-            // Mail::to($adherente->Correo)->send(new EstadosMail($content));
+            Mail::to($adherente->Correo)->send(new EstadosMail($content));
             DB::commit();
             return response()->json([
                 "status" => true,
