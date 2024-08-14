02b6f6609343aff52144ff8023480db5293db785
Juan Felipe Arrieta Ortiz <ticsoft.desarrollador@gmail.com>
2024-08-14 18:38:42 -0500

    Buscar por documento y traer fliares

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -8,9 +8,6 @@ use Illuminate\Support\Facades\Hash;
 
 class UsuarioController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
 
     public function filtroUsuarios()
     {
@@ -33,51 +30,26 @@ class UsuarioController extends Controller
         return response()->json($filteredUsers);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     */
-    public function store(Request $request)
-    {
-        $user = User::create([
-            'Nombre' => $request->Nombre,
-            'Apellidos' => $request->Apellidos,
-            'Fecha' => $request->Fecha,
-            'Edad' => $request->Edad,
-            'Documento' => $request->Documento,
-            'Sexo' => $request->Sexo,
-            'Telefono' => $request->Telefono,
-            'Direccion' => $request->Direccion,
-            'Correo' => $request->Correo,
-            'password' => Hash::make($request->password),
-        ]);
-        return response()->json([
-            "user" => $user
-        ]);
-    }
-
-    /**
-     * Display the specified resource.
-     */
     public function buscarUsuario(String $documento)
     {
         $user = User::with([
-            'admin', 'asociado', 'adherente', 'empleado', 'familiar', 'familiar.adherente', 'familiar.asociado'
+            'admin',
+            'asociado',
+            'adherente',
+            'empleado',
+            'familiar',
+            'familiar.adherente',
+            'familiar.asociado'
         ])->where('Documento', $documento)->first();
         if ($user) {
             if ($user->Rol == 0 || $user->Rol == 1) {
                 $usuario =  $user->admin;
             } else if ($user->Rol == 2) {
                 $usuario =  $user->asociado;
+                $usuario->familiar = $user->asociado->familiares;
             } else if ($user->Rol == 3) {
                 $usuario =  $user->adherente;
+                $usuario->familiar = $user->adherente->familiares;
             } else if ($user->Rol == 4 || $user->Rol == 6) {
                 $usuario =  $user->empleado;
             } else if ($user->Rol == 5) {
@@ -99,17 +71,6 @@ class UsuarioController extends Controller
         }
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     */
-    public function edit(User $user)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     */
     public function cambiarPassword(Request $request, String $id)
     {
         $usuario = User::find($id);
@@ -119,13 +80,5 @@ class UsuarioController extends Controller
         return response()->json([
             "message" => "hecho"
         ], 201);
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(User $user)
-    {
-        //
     }
 }
